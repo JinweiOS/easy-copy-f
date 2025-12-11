bdedf27f3dbdae80a6a8c0a3485f90e0ae80214a
sharecoffee <1962732398@qq.com>
2025-12-11 14:42:21 +0800

    feat: first commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles/_variables.scss'
+import './styles/_keyframe-animations.scss'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
